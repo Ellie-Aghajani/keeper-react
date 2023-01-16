2a4c303e1bc02ea361f1e39dabf12da1ef4d100a
Ellie-Aghajani <z.aqajani@gmail.com>
2023-01-15 22:01:52 -0800

    added components and style

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note(){
+    return(
+        <div className="note">
+            <h1>This is a simple react app</h1>
+            <p>This is a simple react app</p>
+        </div>
+    )
+}
+export default Note; 
